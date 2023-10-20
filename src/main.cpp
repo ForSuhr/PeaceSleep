c650496d9a78f1f01c34adb60ce0038d1168a328
ForSuhr <jiandong.zhao.zier@gmail.com>
2023-10-20 23:49:41 +0200

    Project: add app icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QCoreApplication>
+#include <QIcon>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
@@ -23,6 +24,9 @@ int main(int argc, char* argv[])
         Qt::QueuedConnection);
     engine.rootContext()->setContextProperty("applicationDirPath", QString(QCoreApplication::applicationDirPath()));
     engine.load(url);
+
+    /*window icon*/
+    app.setWindowIcon(QIcon(":/PeaceSleep/ui/assets/appIcon/PeaceSleep.png"));
 
     return app.exec();
 }
